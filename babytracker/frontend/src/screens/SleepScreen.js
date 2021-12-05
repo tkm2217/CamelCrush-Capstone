2b042338eccd982c36cbe7dbb33f445373560bdf
Alimoh99 <46909566+Alimoh99@users.noreply.github.com>
2021-12-04 18:17:12 -0600

    Adding sleep screen components.

--- babytracker/frontend/src/screens/SleepScreen.js
+++ babytracker/frontend/src/screens/SleepScreen.js
@@ -7,3 +7,50 @@ import firebase from "firebase";
 
 import { useUser } from "../hooks/useUser";
 import colors from "../config/colors";
+
+function SleepScreen({ theme, route }) {
+  const [beginDate, setBeginDate] = useState(new Date());
+  const [beginTime, setBeginTime] = useState(new Date());
+  const [endDate, setEndDate] = useState();
+  const [endTime, setEndTime] = useState();
+  const [loading, setLoading] = useState(false);
+  const { selectedChildData } = route.params;
+  const { colors: papercolors } = theme;
+
+  const [showBeginDate, setShowBeginDate] = useState(false);
+  const [showEndDate, setShowEndDate] = useState(false);
+  const [showBeginTime, setShowBeginTime] = useState(false);
+  const [showEndTime, setShowEndTime] = useState(false);
+
+  const { darkTheme, selectedChild } = useUser();
+
+  const handleSave = () => {
+    setLoading(true);
+
+    firebase
+      .firestore()
+      .collection("babies")
+      .doc(selectedChild)
+      .update({
+        sleep: {
+          beginTime: formatAMPM(beginTime),
+          beginDate: beginDate,
+          endTime: formatAMPM(endTime),
+          enddate: endDate,
+        },
+      })
+      .then(() => {
+        firebase.firestore().collection("activities").add({
+          timestamp: new Date(),
+          timestampString: new Date().toDateString(),
+          uid: firebase.auth().currentUser.uid,
+          activity: "Slept",
+          babyName: selectedChildData?.name,
+        });
+
+        Alert.alert("Updated", "Successfully updated sleep details.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
