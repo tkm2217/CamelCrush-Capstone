009b6e90c4baa8e2001bfbf82f0b0c080e3d31fa
tkm2217 <muhammadtalha@my.unt.edu>
2021-12-05 13:32:48 -0600

    Timeline updated

--- babytracker/frontend/src/screens/TimeLineScreen.js
+++ babytracker/frontend/src/screens/TimeLineScreen.js
@@ -109,3 +109,38 @@ function TimeLineScreen({ theme }) {
       </View>
     );
   }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingHorizontal: 10,
+    },
+    titleText: {
+      color: colors.primary,
+      alignSelf: "center",
+      fontWeight: "bold",
+      fontSize: 20,
+    },
+    box: {
+      flex: 1,
+      borderWidth: 2,
+      borderColor: colors.primary,
+      padding: 7,
+      alignItems: "center",
+      height: 60,
+      justifyContent: "center",
+    },
+  });
+  
+  export default withTheme(TimeLineScreen);
+  
+  function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    var strTime = hours + ":" + minutes + " " + ampm;
+    return strTime;
+  }
+  
