8b64bbaad1f1814c396ab6a50ea286a50e50b3a4
Alimoh99 <46909566+Alimoh99@users.noreply.github.com>
2021-12-04 18:21:03 -0600

    Adding CSS

--- babytracker/frontend/src/screens/SleepScreen.js
+++ babytracker/frontend/src/screens/SleepScreen.js
@@ -229,3 +229,32 @@ function SleepScreen({ theme, route }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "grey",
+    flex: 1,
+    margin: 10,
+  },
+  textInput: {
+    backgroundColor: "transparent",
+    flex: 1,
+    margin: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "grey",
+  },
+});
+
+export default withTheme(SleepScreen);
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutes + " " + ampm;
+  return strTime;
+}
